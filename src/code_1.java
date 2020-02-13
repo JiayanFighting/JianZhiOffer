523f2efb80be943ba2b0abd89f4ddae08416d7dd
huangjiayan <jiayanfighting@163.com>
2020-02-13 19:37:12 +0800

    code_1

--- src/code_1.java
+++ src/code_1.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+
 public class code_1 {
     public static void main(String[] args){
-        System.out.println("code_1");
+        int[] nums = {1,2,3,4,5,1};
+        System.out.println(findRepeatNumber(nums));
+    }
+
+    public static int findRepeatNumber(int[] nums) {
+        if (nums.length <= 1) return -1;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int num:nums){
+            int time = map.getOrDefault(num,0);
+            if (time >= 1) return num;
+            map.put(num,time+1);
+        }
+        return -1;
     }
 }
